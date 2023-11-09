09c1a59c3f488b5eb886db25e9e242ba435381a7
kainy01 <kainykawai@gmail.com>
2023-11-09 03:44:56 +0300

    fixed deleted user functions

--- libs/deleteUser.js
+++ libs/deleteUser.js
@@ -1,23 +1,13 @@
-// loginUtils.js
-import Cookies from "js-cookie";
-
 export async function deleteUser(id) {
-  try {
-    const response = await fetch(`https://dummyjson.com/users/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  const response = await fetch(`https://dummyjson.com/users/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (response.ok) {
-      Cookies.remove("userData");
-      return true;
-    } else {
-      return false;
-    }
-  } catch (error) {
-    console.error("Login error:", error);
-    return false;
+  if (!response.ok) {
+    throw new Error("Login failed");
   }
+  return response.json();
 }
